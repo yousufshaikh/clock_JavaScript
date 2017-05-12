b0fecbde709235ad831ef83d982a4c60a1e83699
yousufshaikh <yousufshaikh92@gmail.com>
2017-05-12 21:04:52 +0500

    This is the commit for 12 o Clock

--- app.js
+++ app.js
@@ -14,6 +14,22 @@ function displayClock() {
         dayNight = "PM";
     }
 
+    if (hours == 0) {
+        hours = 12;
+    }
+
+    if (hours < 10) {
+        hours = '0' + hours;
+    }
+
+    if (minutes < 10) {
+        minutes = '0' + minutes;
+    }
+
+    if (seconds < 10) {
+        seconds = '0' + seconds;
+    }
+
     document.getElementById('clock').innerHTML = hours + ':' + minutes + ':' + seconds + ' ' + dayNight;
 }
 setInterval(displayClock, 500);
